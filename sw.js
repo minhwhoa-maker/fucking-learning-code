e3b3bd965739112eaea57eb1c4e528bbe41433ba
fucker <minhwhoa@gmail.com>
2026-05-02 16:18:05 +0700

    add sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'van-tai-v1';
+const ASSETS = [
+  '/bai10.html',
+  '/owner-dashboard.html',
+  '/driver-page.html',
+  '/drivers.html',
+  '/vehicles.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
